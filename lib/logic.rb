a0f4a7f374b81563eff193c0d20cf87196d64ec9
alansoto31 <soto.alan@hotmail.com>
2020-10-29 10:41:48 -0600

    Changed class approach

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,7 +1,32 @@
-class GameStart
+class Strings 
 
-    attr_accessor :grid
-    
+    def rules
+        "
+    Rules
+
+1. The game shows a grid of 3 x 3 spaces with a number for every single available space.
+2. Players take turns to choose between 1...9 options. There are 2 marks, X for player 1 and O for player 2.
+3. When a player gets 3 marks in one line (row, column or diagonall), He is the WINNER.
+4. The game ends with a DRAW when the whole grid is full and none players has a 3 marks in one line.
+        "
+    end
+end
+
+class Players
+
+    def check_answer(user_answer)
+        if %w[Y y].include?(user_answer) == true
+            $game_on = true
+        end
+    end
+
+    def check_input(user_input)
+        true if user_input.between?(1,9)
+    end
+end
+
+class Grid
+
     def initialize
         @grid = ["1", "2", "3", "4", "5", "6", "7", "8" , "9"]
     end
@@ -15,39 +40,14 @@ class GameStart
          #{@grid[6]} | #{@grid[7]} | #{@grid[8]}
         " 
     end
-    def rules
-        "
-    Rules
-
-1. The game shows a grid of 3 x 3 spaces with a number for every single available space.
-2. Players take turns to choose between 1...9 options. There are 2 marks, X for player 1 and O for player 2.
-3. When a player gets 3 marks in one line (row, column or diagonall), He is the WINNER.
-4. The game ends with a DRAW when the whole grid is full and none players has a 3 marks in one line.
-        "
-    end
-end
-
-class CheckInputs
-
-    def check_answer(user_answer)
-        if %w[Y y].include?(user_answer) == true
-            $game_on = true
-        end
-    end
-
-    def check_input(user_input)
-        true if user_input.between?(1,9)
-    end
-end
-
-class GameLogic
     
-    def assigning_marks(game, user_input, n)
+    def assigning_marks(user_input, n)
         user_input -= 1
         if n == 1
-            game.grid[user_input] = "X"
+            @grid[user_input] = "X"
         else 
-            game.grid[user_input] = "O"
+            @grid[user_input] = "O"
         end
     end
+    
 end
